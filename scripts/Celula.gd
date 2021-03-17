98701796cf646ce2ab0116144d5a3847cfb796cc
brunoclaudino <38812683+brunoclaudino@users.noreply.github.com>
2021-03-17 17:06:19 -0300

    Tentando fazer um formato rendondo

--- scripts/Celula.gd
+++ scripts/Celula.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var organela = preload("res://scenes/Organelas.tscn")
-var raio = 100
+var raio = 300
 var y = 0.0
 var x = 0.0
 
@@ -13,12 +13,21 @@ func _ready():
 
 func gerar_membrana():
 	var i = 0
-	while i < 250:
+	while i < 10:
 		y = sqrt((raio*raio + x*x))
 		var temp = organela.instance()
-		get_parent().add_child(temp)
-		temp.position.y = y
-		temp.position.x = x
-		x += 0.4
+		add_child(temp)
+		temp.set_position(Vector2((700+x), y))
+		temp = organela.instance()
+		add_child(temp)
+		temp.set_position(Vector2((700-x), y))
+		temp = organela.instance()
+		add_child(temp)
+		temp.set_position(Vector2((700-x), 2*raio-y))
+		temp = organela.instance()
+		add_child(temp)
+		temp.set_position(Vector2((700+x), 2*raio-y))
+		x += 30
 		i += 1
+
 
--- scripts/Organelas.gd
+++ scripts/Organelas.gd
@@ -9,7 +9,7 @@ var descri
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Sprite.texture = textura
-	pass # Replace with function body.
+	
 
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
