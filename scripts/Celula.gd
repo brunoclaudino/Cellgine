bc3cd7b27bae9f90e06425f6699d0a4b037c4156
brunoclaudino <38812683+brunoclaudino@users.noreply.github.com>
2021-04-29 18:46:23 -0300

    Alguns sprites e colocações

--- scripts/Celula.gd
+++ scripts/Celula.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var organela = preload("res://scenes/Organelas.tscn")
+var protein = preload("res://scenes/proteina.tscn")
 var lista = preload("res://scripts/ActionList.gd")
 
 #lita de caminhos para fazer load nas estruturas sem ter que deixar tudo em preload
@@ -12,12 +12,14 @@ var inst_organ = ["res://scenes/Nucleo.tscn", "res://scenes/Golgi.tscn", "res://
 # Nucleo, golgi, cloroplastos, Lisossomos, Mitocondria, Peroxissomos, REL, RER, Ribossomos, Vacuolo
 # Filamentos, Microfilamentos, Microtubulos
 var estruturas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]                        # Guarda quais e quantas estruturas tem
-var raio = 300
+var p = protein.instance()
 var y = 0.0
 var x = 0.0
 
 
 func _ready():
+	$Membrana.add_child(p)
+	p.position = $Position2D.position + Vector2(-750,0)
 	var temp = lista.new()                                                      # Testando add coisas na lista
 	temp.add_lista(1,3,55)
 	temp.add_lista(1, 2, 15)
@@ -26,6 +28,7 @@ func _ready():
 
 func voltar_textura():                                                          # Volta a textura para a default
 	$Membrana.play("default", false)
+	p.visivel()
 
 func mudar_pos(inicial, altX, altY):                                            # Método para mudar posição mais facilmente por de fora da célula
 	var temp = inicial
@@ -34,6 +37,7 @@ func mudar_pos(inicial, altX, altY):                                            
 
 func membr_invisivel():                                                         # Muda para uma animação sem textura
 	$Membrana.play("invisivel", false)
+	p.invisivel()
 
 func add_estruturas():                                                          # Func para colocar as organelas
 	var i = 0
@@ -45,25 +49,55 @@ func add_estruturas():                                                          
 func carregar_estrutura(index):                                                 # Switch para chamada de criação de organelas
 	match index:
 		0:
-			pass
+			var nucleo = load('res://scenes/Nucleo.tscn')
+			var temp = nucleo.instance()
+			add_child(temp)
+			temp.position = $Position2D.position
 		1:
-			pass
+			var golgi = load('res://scenes/Golgi.tscn')
+			var temp = golgi.instance()
+			add_child(temp)
+			temp.position = $Position2D.position + Vector2(0, 250)
 		2:
-			pass
+			var clorop = load('res://scenes/Cloroplastos.tscn')
+			var temp = clorop.instance()
+			add_child(temp)
+			temp.position = $Position2D.position + Vector2(0, 250)
 		3:
-			pass
+			var liso = load('res://scenes/Lisossomos.tscn')
+			var temp = liso.instance()
+			add_child(temp)
+			temp.position = $Position2D.position + Vector2(200, 200)
 		4:
-			pass
+			var mito = load('res://scenes/Mitocondria.tscn')
+			var temp = mito.instance()
+			add_child(temp)
+			temp.position = $Position2D.position + Vector2(-100, -300)
 		5:
-			pass
+			var peri = load('res://scenes/Peroxissomos.tscn')
+			var temp = peri.instance()
+			add_child(temp)
+			temp.position = $Position2D.position + Vector2(-300, -300)
 		6:
-			pass
+			var rel = load('res://scenes/REL.tscn')
+			var temp = rel.instance()
+			add_child(temp)
+			temp.position = $Position2D.position + Vector2(100, 300)
 		7:
-			pass
+			var rer = load('res://scenes/RER.tscn')
+			var temp = rer.instance()
+			add_child(temp)
+			temp.position = $Position2D.position + Vector2(-200, 300)
 		8:
-			pass
+			var ribo = load('res://scenes/Ribossomos.tscn')
+			var temp = ribo.instance()
+			add_child(temp)
+			temp.position = $Position2D.position + Vector2(-100, 0)
 		9:
-			pass
+			var vac = load('res://scenes/Vacuolo.tscn')
+			var temp = vac.instance()
+			add_child(temp)
+			temp.position = $Position2D.position + Vector2(200, 200)
 		10:
 			pass
 		11:
---
+++ scripts/proteina.gd
@@ -0,0 +1,37 @@
+extends Node
+
+var textura = preload("res://icon.png")
+var id
+var nome
+var descri
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	
+
+func invisivel():
+	$Textura.play('invisivel')
+
+func visivel():
+	$Textura.play('default')
+	
+func bomba_Na_Cl(quant):                                                        # Func de controle da animação da bomba
+	if quant> 0:                                                                # quant recebe a quantidade de vezes que se deseja que a animação seja feita
+		var i = 0                                                               # i controla a repetição do laço
+		while(i<quant):                                                         # 
+			$Textura.play('bombaNaCl')                                          # Troca a animação para a da bomba
+			while($Textura.is_playing()):                                       # Espera a animação trocar dentro desse laço
+				pass
+			i += 1
+	elif quant == 0:                                                            # Se quant for = 0, a animação ocorre pra sempre
+		while true:
+			$Texttura.play('bombaNaCl')
+			while($Textura.is_playing()):
+				pass
+	else:                                                                       # Se o valor for < 0, erro!
+		print('Valor inserido é Inválido')
+		return 'error'
+	$Textura.play('default')
+	return 'success'
