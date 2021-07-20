fa3cc7b3fc96440c61f213dcddefbc31623a90f2
Victória Gomes <victoria.oliveiragomes@gmail.com>
2021-07-20 16:00:16 -0300

    Modificações para movimentação correta da célula procarionte

--- scripts/Procarionte.gd
+++ scripts/Procarionte.gd
@@ -5,26 +5,8 @@ var lista = preload("res://scripts/ActionList.gd")
 # DNA, Plasmídeos, Ribossomos
 var estruturas = [0, 0, 0]                        # Guarda quais e quantas estruturas tem
 #var p = protein.instance()
-var mouse_entrou = false
-var drag_enabled = false
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-func _input(event : InputEvent):
-	if event is InputEventMouseButton:
-		if event.is_pressed(): 
-			if event.button_index == BUTTON_WHEEL_UP: # zoom in
-				self.scale += Vector2(0.1, 0.1)
-				# call the zoom function
-			if event.button_index == BUTTON_WHEEL_DOWN: # zoom out
-				self.scale -= Vector2(0.1, 0.1)
-				# call the zoom function
-	if mouse_entrou && Input.is_action_pressed("left_click"):
-		self.position = event.position
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+var selected = false
+var mouse_in = false
 
 func voltar_textura():                                                          # Volta a textura para a default
 	$Membrana.play("default", false)
@@ -85,9 +67,37 @@ func carregar_estrutura(index):                                                 
 			print("Valor inesperado de index de estrutura")
 
 
+# Movimentação da célula
+
+var grabbed_offset = Vector2()
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("left_click"):
+		grabbed_offset = global_position - get_global_mouse_position()
+
+func _physics_process(delta):
+	if selected:
+		global_position = lerp(global_position, get_global_mouse_position() + grabbed_offset, 25 * delta)
+
+func on_click():
+	selected = true
+
+func _input(event : InputEvent):
+	if event is InputEventMouseButton:
+		if event.is_pressed() and mouse_in: 
+			if event.button_index == BUTTON_WHEEL_UP: # zoom in
+				self.scale += Vector2(0.1, 0.1)
+				# call the zoom function
+			if event.button_index == BUTTON_WHEEL_DOWN: # zoom out
+				self.scale -= Vector2(0.1, 0.1)
+				# call the zoom function
+		elif event.button_index == BUTTON_LEFT and not event.pressed:
+			selected = false
+
+
 func _on_Area2D_mouse_entered():
-	mouse_entrou = true
+	mouse_in = true
 
 
 func _on_Area2D_mouse_exited():
-	mouse_entrou = false
+	mouse_in = false
